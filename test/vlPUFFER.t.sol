b416316387a4d77e7e73f64628e093d4841fb14f
Benjamin <benjaminxh+github@gmail.com>
2025-05-07 13:33:59 +0200

    change casing

--- test/vlPUFFER.t.sol
+++ test/vlPUFFER.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import { vlPUFFER } from "../src/vlPuffer.sol";
+import { vlPUFFER } from "../src/vlPUFFER.sol";
 import { ERC20PermitMock } from "./mocks/ERC20PermitMock.sol";
 import { Test } from "forge-std/Test.sol";
 
